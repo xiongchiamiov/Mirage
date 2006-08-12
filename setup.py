d7df5f333671a1a453e42bbbe16363514881e04c
stonecrest <stonecrest@c8e92e14-2b13-0410-9bd1-9f109af9afc5>
2006-08-12 17:36:03 +0000

    check gtk version upon install
    git-svn-id: http://svn.berlios.de/svnroot/repos/mirageiv/trunk@88 c8e92e14-2b13-0410-9bd1-9f109af9afc5

--- setup.py
+++ setup.py
@@ -1,29 +1,38 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension
+import gtk
 
-setup(name='Mirage',
-        version='0.7.3',
-        description='A fast GTK+ image viewer',
-        author='Scott Horowitz',
-        author_email='stonecrest@gmail.com',
-        url='http://mirageiv.berlios.de',
-        classifiers=[
-            'Environment :: X11 Applications',
-            'Intended Audience :: End Users/Desktop',
-            'License :: GNU General Public License (GPL)',
-            'Operating System :: Linux',
-            'Programming Language :: Python',
-            'Topic :: Multimedia :: Graphics :: Viewers'
-            ],
-        py_modules = ['mirage'],
-	ext_modules = [Extension('imgfuncs', ['imgfuncs.c'])],
-        scripts = ['mirage'],
-        data_files=[('share/mirage', ['README', 'COPYING', 'CHANGELOG', 'stock_shuffle.png']),
-                    ('share/applications', ['mirage.desktop']),
-                    ('share/pixmaps', ['mirage.png', 'mirage_large.png']),
-		    ('share/locale/pl/LC_MESSAGES', ['locale/pl/LC_MESSAGES/mirage.mo']),
-		    ('share/locale/fr/LC_MESSAGES', ['locale/fr/LC_MESSAGES/mirage.mo']),
-		    ('share/locale/es/LC_MESSAGES', ['locale/es/LC_MESSAGES/mirage.mo']),
-		    ('share/locale/de/LC_MESSAGES', ['locale/de/LC_MESSAGES/mirage.mo'])],
-        )
+try:
+	test = gtk.Button("", gtk.STOCK_FULLSCREEN)
+except:
+	print "Error: Mirage requires gtk+ 2.8.x, which you do not appear to have installed."
+	test = None
+
+if test != None:
+	from distutils.core import setup, Extension
+
+	setup(name='Mirage',
+			version='0.7.3',
+			description='A fast GTK+ image viewer',
+			author='Scott Horowitz',
+			author_email='stonecrest@gmail.com',
+			url='http://mirageiv.berlios.de',
+			classifiers=[
+				'Environment :: X11 Applications',
+				'Intended Audience :: End Users/Desktop',
+				'License :: GNU General Public License (GPL)',
+				'Operating System :: Linux',
+				'Programming Language :: Python',
+				'Topic :: Multimedia :: Graphics :: Viewers'
+				],
+			py_modules = ['mirage'],
+		ext_modules = [Extension('imgfuncs', ['imgfuncs.c'])],
+			scripts = ['mirage'],
+			data_files=[('share/mirage', ['README', 'COPYING', 'CHANGELOG', 'stock_shuffle.png']),
+						('share/applications', ['mirage.desktop']),
+						('share/pixmaps', ['mirage.png', 'mirage_large.png']),
+				('share/locale/pl/LC_MESSAGES', ['locale/pl/LC_MESSAGES/mirage.mo']),
+				('share/locale/fr/LC_MESSAGES', ['locale/fr/LC_MESSAGES/mirage.mo']),
+				('share/locale/es/LC_MESSAGES', ['locale/es/LC_MESSAGES/mirage.mo']),
+				('share/locale/de/LC_MESSAGES', ['locale/de/LC_MESSAGES/mirage.mo'])],
+			)
