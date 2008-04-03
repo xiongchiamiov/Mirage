9aa710d360875f2dc905c8d13178ea568832e925
stonecrest <stonecrest@c8e92e14-2b13-0410-9bd1-9f109af9afc5>
2008-04-03 15:07:27 +0000

    oops, fix r242; initialize var to 0, not None
    git-svn-id: http://svn.berlios.de/svnroot/repos/mirageiv/trunk@243 c8e92e14-2b13-0410-9bd1-9f109af9afc5

--- mirage.py
+++ mirage.py
@@ -172,7 +172,7 @@ class Base:
 		self.thumbpane_updating = False
 		self.recentfiles = ["", "", "", "", ""]
 		self.screenshot_delay = 2
-		self.thumbpane_bottom_coord_loaded = None
+		self.thumbpane_bottom_coord_loaded = 0
 
 		# Read any passed options/arguments:
 		try:
@@ -810,9 +810,8 @@ class Base:
 		# Load all images up to the bottom ofo the visible thumbpane rect:
 		rect = self.thumbpane.get_visible_rect()
 		bottom_coord = rect.y + rect.height + self.thumbnail_size
-		if self.thumbpane_bottom_coord_loaded:
-			if bottom_coord > self.thumbpane_bottom_coord_loaded:
-				self.thumbpane_bottom_coord_loaded = bottom_coord
+		if bottom_coord > self.thumbpane_bottom_coord_loaded:
+			self.thumbpane_bottom_coord_loaded = bottom_coord
 		# update images:
 		if not self.thumbpane_updating:
 			thread = threading.Thread(target=self.thumbpane_update_pending_images, args=(force_upto_imgnum, None))
